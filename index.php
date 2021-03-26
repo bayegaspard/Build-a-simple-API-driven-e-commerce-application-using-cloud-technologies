a81d713631fb5a130aa99903331ddc18b0219b36
bayegaspard <gaspybaye@gmail.com>
2021-03-26 03:34:08 -0400

    project coursera files

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Simple e-commerce application</title>
+    </head>
+
+    <body>
+        <h1>Welcome to my simple e-commerce site</h1>
+        <ul>
+  
+  <! -- php code here --> 
+  
+        </ul>
+    </body>
+</html>
